084e1b1cd0131efd066fee4df589f95e4d25ceb6
Edomiyas Tesfaye Zelleke <164158449+EdomiyasTesfaye@users.noreply.github.com>
2025-03-19 00:35:03 +0300

    man

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Edomiyas Tesfaye Zelleke",
+  name: "Edomiyas Zelleke",
   initials: "Hammer",
   url: "https://edomiyastesfaye.vercel.app/",
   location: "Ethiopia, AA",
